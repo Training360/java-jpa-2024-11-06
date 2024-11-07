cd73c7e5aff77e80481e1e7abe5c63c2f0acd560
vicziani <viczian.istvan@gmail.com>
2024-11-07 12:43:35 +0100

    Lekérdezés

--- employees-jpa/src/main/java/employees/EmployeesApplication.java
+++ employees-jpa/src/main/java/employees/EmployeesApplication.java
@@ -1,8 +1,21 @@
 package employees;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+import java.util.List;
+
 public class EmployeesApplication {
 
     public static void main(String[] args) {
+        try (
+                EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("pu");
+                EntityManager em = entityManagerFactory.createEntityManager()) {
+            // JPQL
+            List<Employee> employees = em.createQuery("select e from Employee e", Employee.class).getResultList();
+            employees.stream().map(Employee::getName).forEach(System.out::println);
+        }
 
     }
 }
